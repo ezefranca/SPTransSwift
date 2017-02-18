46b65a3c6aae02a69f7ab010f535cfec7af508e7
ezefranca <ezequiel.ifsp@gmail.com>
2017-02-18 19:34:14 -0200

    added some methods

--- SPTransSwift/Example/SPTransSwiftExample/ViewController.swift
+++ SPTransSwift/Example/SPTransSwiftExample/ViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
+import SPTransSwift
 
 class ViewController: UIViewController {
-
+    
+    var a:SPTransClient = SPTransClient(athenticationToken: "")
     override func viewDidLoad() {
         super.viewDidLoad()
+        a.autenticar(athenticationToken:"76139b0b198f36ccbba8d8004fd176fb56bba86bd020684aa68716bd7d259483")
+        
+        print("autenticado")
+        
+
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func pegarCorredores(_ sender: Any) {
+        a.pegarCorredores() {_,_,_ in
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
--- SPTransSwift/SPTransSwift/Classes/SPTransClient.swift
+++ SPTransSwift/SPTransSwift/Classes/SPTransClient.swift
@@ -10,23 +10,71 @@ import Foundation
 
 public struct SPTransClient {
     
+    public typealias RequestCompletion = (Data?, URLResponse?, Error?) -> Void
     private var requester: Requester
 
     public init(athenticationToken: String) {
         requester = Requester(athenticationToken: athenticationToken)
     }
     
-    private func athenticationURL(base:String, token:String) -> String {
-        return base + "/Login/Autenticar?token=" + token
+    private func athenticationURL(token:String) -> String {
+        return "/Login/Autenticar?token=" + token
     }
     
-    func athentication(athenticationToken: String){
+    public func autenticar(athenticationToken: String){
         
-        requester.sendRequest(method: .post, endpoint: athenticationURL(base: requester.baseURL, token: athenticationToken), headers: [:], body: "") {
+        requester.sendRequest(method: .post, endpoint: athenticationURL(token: athenticationToken), headers: [:], body: "") {
             (data, response, error) in
             print(data)
             print(response)
             return
         }
     }
+    
+    private func genericGET(_ endpoint:String, completionHandler: @escaping RequestCompletion) {
+        requester.sendRequest(method: .get, endpoint: endpoint, headers: [:], body: "") {
+            (data, response, error) in
+            print(data)
+            print(response)
+            completionHandler(data, response, error)
+            return
+        }
+    }
+    
+    func buscarDetalheLinha(termosBusca: String, completionHandler: @escaping RequestCompletion) {
+        genericGET("/Linha/Buscar?termosBusca=" + termosBusca, completionHandler: completionHandler)
+    }
+    
+    func carregarDetalhesLinha(codigoLinha: String, completionHandler: @escaping RequestCompletion) {
+        genericGET("/Linha/CarregarDetalhes?codigoLinha=" + codigoLinha, completionHandler: completionHandler)
+    }
+    
+    func buscarParada(termosBusca: String, completionHandler: @escaping RequestCompletion) {
+        genericGET("/Parada/Buscar?termosBusca=" + termosBusca, completionHandler: completionHandler)
+    }
+    
+    func buscarParadaPorLinha(codigoLinha: String, completionHandler: @escaping RequestCompletion) {
+        genericGET("/Parada/BuscarParadasPorLinha?codigoLinha=" + codigoLinha, completionHandler: completionHandler)
+    }
+    
+    func buscarParadaPorCorredor(codigoCorredor: String, completionHandler: @escaping RequestCompletion) {
+        genericGET("/Parada/BuscarParadasPorCorredor?codigoCorredor=" + codigoCorredor, completionHandler: completionHandler)
+    }
+    
+    public func pegarCorredores(completionHandler: @escaping RequestCompletion) {
+        genericGET("/Corredor", completionHandler: completionHandler)
+    }
+    
+    func pegarPosicaoOnibus(codigoLinha: String, completionHandler: @escaping RequestCompletion) {
+        genericGET("/Posicao?codigoLinha=" + codigoLinha, completionHandler: completionHandler)
+    }
+    
+    func pegarTempoDeChegadaDeUmOnibusNaParada(codigoLinha: String, codigoParada: String, completionHandler: @escaping RequestCompletion) {
+        genericGET("/Previsao?codigoParada=" + codigoParada + "&codigoLinha=" + codigoLinha, completionHandler: completionHandler)
+    }
+    
+    func pegarTemposDeChegadaParada(codigoParada: String, completionHandler: @escaping RequestCompletion) {
+        genericGET("/Previsao/Parada?codigoParada=" + codigoParada, completionHandler: completionHandler)
+    }
+    
 }
